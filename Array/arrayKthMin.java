73f5687b2b2910fdbd1f1cf8cbb9a7d32853eb5e
Abhishek(Icarus) <east7891@gmail.com>
2021-10-18 22:40:12 +0530

    Added some more array based

--- Array/arrayKthMin.java
+++ Array/arrayKthMin.java
@@ -1,6 +1,53 @@
-public class arrayKthMin {
-    public static void main(String args[]) {
+import java.util.Random;
 
+public class arrayKthMin {
+
+    static void swap(int arr[], int l, int r) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+
+    static int partition(int arr[], int l, int r) {
+        int pivot = arr[r];
+        int i = l;
+        for (int j = l; j <= r - 1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, l, r);
+        return i;
+    }
+
+    static int randomPivot(int arr[], int l, int r) {
+        int range = r - l + 1;
+        Random randomNo = new Random();
+        int pivot = randomNo.nextInt(range);
+        swap(arr, l + pivot, r);
+        return partition(arr, l, r);
+
+    }
+
+    static int kthMin(int arr[], int l, int r, int k) {
+        if (k > 0 && k <= r - l + 1) {
+            int pivPos = randomPivot(arr, l, r);
+
+            if (pivPos - l == k - 1)
+                return arr[pivPos];
+            if (pivPos - l > k - 1)
+                return kthMin(arr, l, pivPos - 1, k);
+            return kthMin(arr, pivPos + 1, r, k - pivPos + l - 1);
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 1, 4, 3, 2, 6, 5 };
+        int size = numbers.length;
+        int KthSmall = kthMin(numbers, 0, size - 1, 5);
+        System.out.println("The kth smallest element in the array is " + KthSmall);
     }
 
 }
---
+++ Array/arraySort.java
@@ -0,0 +1,35 @@
+class arraySort {
+
+    static void sort012(int arr[], int size) {
+        int numofZeros = 0, numofOnes = 0, numofTwos = 0;
+        for (int el : arr) {
+            if (el == 0) {
+                numofZeros++;
+            } else if (el == 1) {
+                numofOnes++;
+            } else {
+                numofTwos++;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (i < numofZeros) {
+                arr[i] = 0;
+            } else if (i < numofZeros + numofOnes) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 2;
+            }
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 0, 1, 2, 2, 1, 0, 2 };
+        sort012(numbers, numbers.length);
+        for (int x : numbers) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+}
