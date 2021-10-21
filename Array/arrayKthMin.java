ca0663b900bd880ecc4c45af84d23b583ced5289
Abhishek(Icarus) <east7891@gmail.com>
2021-10-21 22:32:54 +0530

    Fixed arrayKthMin

--- Array/arrayKthMin.java
+++ Array/arrayKthMin.java
@@ -1,53 +1,20 @@
-import java.util.Random;
+import java.util.*;
 
 public class arrayKthMin {
-
-    static void swap(int arr[], int l, int r) {
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
-    }
-
-    static int partition(int arr[], int l, int r) {
-        int pivot = arr[r];
-        int i = l;
-        for (int j = l; j <= r - 1; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                swap(arr, i, j);
+    static int kthSmallest(int arr[], int k) {
+        PriorityQueue<Integer> maxQ = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for (int el : arr) {
+            maxQ.add(el);
+            if (maxQ.size() > k) {
+                maxQ.poll();
             }
         }
-        swap(arr, l, r);
-        return i;
+        return maxQ.poll();
     }
 
-    static int randomPivot(int arr[], int l, int r) {
-        int range = r - l + 1;
-        Random randomNo = new Random();
-        int pivot = randomNo.nextInt(range);
-        swap(arr, l + pivot, r);
-        return partition(arr, l, r);
-
+    public static void main(String[] args) {
+        int numbers[] = { 7, 10, 4, 3, 20, 15 };
+        int KthsmallestNumber = kthSmallest(numbers, 3);
+        System.out.println("kth smallest number is " + KthsmallestNumber);
     }
-
-    static int kthMin(int arr[], int l, int r, int k) {
-        if (k > 0 && k <= r - l + 1) {
-            int pivPos = randomPivot(arr, l, r);
-
-            if (pivPos - l == k - 1)
-                return arr[pivPos];
-            if (pivPos - l > k - 1)
-                return kthMin(arr, l, pivPos - 1, k);
-            return kthMin(arr, pivPos + 1, r, k - pivPos + l - 1);
-        }
-        return Integer.MAX_VALUE;
-    }
-
-    public static void main(String args[]) {
-        int numbers[] = { 1, 4, 3, 2, 6, 5 };
-        int size = numbers.length;
-        int KthSmall = kthMin(numbers, 0, size - 1, 5);
-        System.out.println("The kth smallest element in the array is " + KthSmall);
-    }
-
 }
